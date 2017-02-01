90b3cbb44ceebe36ac3dc820404b05915040e2af
jaketaylorpro <jaketaylorpro@gmail.com>
2017-01-31 22:21:10 -0500

    removed unsupported object and array types

--- test-echo/cmd.py
+++ test-echo/cmd.py
@@ -1,14 +1,12 @@
-import base64
 import os
 import sys
 import functools
-from PIL import Image
 from dateutil import parser as dateutil_parser
 import simplejson as json
 
 
 def build_path(path):
-    if os.environ['MCCOY_DEV'] == 'true':
+    if 'MCCOY_DEV' in os.environ and os.environ['MCCOY_DEV'] == 'true':
         return functools.reduce(
             lambda acc, v: acc + '\\' + v,
             filter(lambda o: len(o) > 0, path.split('/')),
@@ -28,19 +26,21 @@ schema = {
     "name": "test_echo",
     "type": "object",
     "properties": {
-        "test-string": {"type": "string"},
-        "test-date": {"type": "string", "format": "date-time"},
-        "test.jpg": {"type": "string", "format": "image/jpg"},
-        "test-integer": {"type": "integer"},
-        "test-float": {"type": "number"},
-        "test-object": {
-            "type": "object",
-            "properties": {
-                "test-object-bool": {"type": "boolean"},
-                "test-object-enum": {"type": "string", "enum": ["A", "B", "C"]},
-                "test-array": {"type": "array", "items": {"type": "string", "format": "uri"}}
-            }
-        }
+        "test-string": {"type": "string", "name": "test-string"},
+        "test-date": {"type": "string", "format": "date-time", "name": "test-date"},
+        "test.jpg": {"type": "string", "format": "image/jpg", "name": "test.jpg"},
+        "test-integer": {"type": "integer", "name": "test-integer"},
+        "test-float": {"type": "number", "name": "test-float"},
+        "test-bool": {"type": "boolean", "name": "test-bool"},
+        "test-enum": {"type": "string", "enum": ["A", "B", "C"], "name": "test-enum"},
+        # "test-array": {"type": "array", "items": {"type": "string", "format": "uri"}, "name": "test-array"},
+        # "test-object": {
+        #     "type": "object",
+        #     "properties": {
+        #         "test-object-string": {"type": "string", "name": "test-object-string"},
+        #     },
+        #     "name": "test-object"
+        # }
     }
 }
 # respond to the metadata prompt by writing to stdout, with the id, schemas, and info
@@ -81,8 +81,10 @@ with open_path('/mccoy/input/test-date', 'r') as file_in, \
     test_date = dateutil_parser.parse(test_date_string)
     file_out.write(str(test_date))
 
-with Image.open(build_path('/mccoy/input/test.jpg'), 'r') as file_in:
-    file_in.save(build_path('/mccoy/output/test.jpg'))
+with open_path('/mccoy/input/test.jpg', 'rb') as file_in, \
+        open_path('/mccoy/output/test.jpg', 'wb') as file_out:
+    data = file_in.read()
+    file_out.write(data)
 
 with open_path('/mccoy/input/test-integer', 'r') as file_in, \
         open_path('/mccoy/output/test-integer', 'w') as file_out:
@@ -96,8 +98,6 @@ with open_path('/mccoy/input/test-float', 'r') as file_in, \
     test_float = float(test_float_string)
     file_out.write(str(test_float))
 
-# make the dir to hold test object
-os.mkdir(build_path('/mccoy/output/test-object'))
 
 with open_path('/mccoy/input/test-object/test-object-bool', 'r') as file_in, \
         open_path('/mccoy/output/test-object/test-object-bool', 'w') as file_out:
@@ -110,20 +110,40 @@ with open_path('/mccoy/input/test-object/test-object-enum', 'r') as file_in, \
     test_enum = file_in.read()
     file_out.write(test_enum)
 
-# make the dir to hold test array
-os.mkdir(build_path('/mccoy/output/test-object/test-array'))
 
-with open_path('/mccoy/input/test-object/test-array/0', 'r') as file_in_0, \
-        open_path('/mccoy/input/test-object/test-array/1', 'r') as file_in_1, \
-        open_path('/mccoy/input/test-object/test-array/2', 'r') as file_in_2, \
-        open_path('/mccoy/output/test-object/test-array/0', 'w') as file_out_0, \
-        open_path('/mccoy/output/test-object/test-array/1', 'w') as file_out_1, \
-        open_path('/mccoy/output/test-object/test-array/2', 'w') as file_out_2:
-    test_uri_0 = file_in_0.read()
-    test_uri_1 = file_in_1.read()
-    test_uri_2 = file_in_2.read()
-    file_out_0.write(test_uri_0)
-    file_out_1.write(test_uri_1)
-    file_out_2.write(test_uri_2)
+with open_path('/mccoy/input/test-object-bool', 'r') as file_in, \
+        open_path('/mccoy/output/test-object-bool', 'w') as file_out:
+    test_bool_string = file_in.read()
+    test_bool = test_bool_string == 'True'
+    file_out.write(str(test_bool))
+
+with open_path('/mccoy/input/test-object-enum', 'r') as file_in, \
+        open_path('/mccoy/output/test-object-enum', 'w') as file_out:
+    test_enum = file_in.read()
+    file_out.write(test_enum)
+
+# # make the dir to hold test array
+# os.mkdir(build_path('/mccoy/output/test-array'))
+#
+# with open_path('/mccoy/input/test-array/0', 'r') as file_in_0, \
+#         open_path('/mccoy/input/test-array/1', 'r') as file_in_1, \
+#         open_path('/mccoy/input/test-array/2', 'r') as file_in_2, \
+#         open_path('/mccoy/output/test-array/0', 'w') as file_out_0, \
+#         open_path('/mccoy/output/test-array/1', 'w') as file_out_1, \
+#         open_path('/mccoy/output/test-array/2', 'w') as file_out_2:
+#     test_uri_0 = file_in_0.read()
+#     test_uri_1 = file_in_1.read()
+#     test_uri_2 = file_in_2.read()
+#     file_out_0.write(test_uri_0)
+#     file_out_1.write(test_uri_1)
+#     file_out_2.write(test_uri_2)
+#
+# # make the dir to hold test object
+# os.mkdir(build_path('/mccoy/output/test-object'))
+#
+# with open_path('/mccoy/input/test-object/test-string', 'r') as file_in, \
+#         open_path('/mccoy/output/test-object/test-string', 'w') as file_out:
+#     test_string = file_in.read()
+#     file_out.write(test_string)
 
 exit(0)
