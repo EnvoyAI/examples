911b003281faaa36e70217572f1f008d80e482c8
EnvoyAI Dev <dev@envoyai.com>
2017-02-08 20:47:00 -0500

    fixed bugs, added percentage, changed _format to format

--- test-echo/cmd.py
+++ test-echo/cmd.py
@@ -15,11 +15,12 @@ def build_path(path):
         return path
 
 
-test = build_path('/mccoy/input/bla')
-
-
 def open_path(path, mode):
     return open(build_path(path), mode=mode)
+
+
+def does_path_exist(path):
+    return os.path.exists(build_path(path))
 
 
 schema = {
@@ -28,9 +29,10 @@ schema = {
     "properties": {
         "test-string": {"type": "string", "title": "test-string"},
         "test-date": {"type": "string", "format": "date-time", "title": "test-date"},
-        "test.jpg": {"type": "string", "format": "image/jpg", "title": "test.jpg"},
+        "test.jpg": {"type": "string", "format": "base64", "title": "test.jpg", "_mime-type": "image/jpg"},
         "test-integer": {"type": "integer", "title": "test-integer"},
         "test-float": {"type": "number", "title": "test-float"},
+        "test-percentage": {"type": "number", "title": "test-percentage", "format": "percentage"},
         "test-bool": {"type": "boolean", "title": "test-bool"},
         "test-enum": {"type": "string", "enum": ["A", "B", "C"], "title": "test-enum"},
         # "test-array": {"type": "array", "items": {"type": "string", "format": "uri"}, "title": "test-array"},
@@ -68,61 +70,59 @@ if len(sys.argv) > 1:
     sys.stderr.write('bad arguments')
     sys.exit(1)
 
-print('debug')
-print(str(os.listdir('/mccoy/input')))
-print('/debug')
-
 # if no args were passed we read from /input and write to /output
+if does_path_exist('/mccoy/input/test-string'):
+    with open_path('/mccoy/input/test-string', 'r') as file_in, \
+            open_path('/mccoy/output/test-string', 'w') as file_out:
+        test_string = file_in.read()
+        file_out.write(test_string)
 
-with open_path('/mccoy/input/test-string', 'r') as file_in, \
-        open_path('/mccoy/output/test-string', 'w') as file_out:
-    test_string = file_in.read()
-    file_out.write(test_string)
+if does_path_exist('/mccoy/input/test-date'):
+    with open_path('/mccoy/input/test-date', 'r') as file_in, \
+            open_path('/mccoy/output/test-date', 'w') as file_out:
+        test_date_string = file_in.read()
+        test_date = dateutil_parser.parse(test_date_string)
+        file_out.write(str(test_date))
 
-with open_path('/mccoy/input/test-date', 'r') as file_in, \
-        open_path('/mccoy/output/test-date', 'w') as file_out:
-    test_date_string = file_in.read()
-    test_date = dateutil_parser.parse(test_date_string)
-    file_out.write(str(test_date))
+if does_path_exist('/mccoy/input/test.jpg'):
+    with open_path('/mccoy/input/test.jpg', 'rb') as file_in, \
+            open_path('/mccoy/output/test.jpg', 'wb') as file_out:
+        data = file_in.read()
+        file_out.write(data)
 
-with open_path('/mccoy/input/test.jpg', 'rb') as file_in, \
-        open_path('/mccoy/output/test.jpg', 'wb') as file_out:
-    data = file_in.read()
-    file_out.write(data)
+if does_path_exist('/mccoy/input/test-integer'):
+    with open_path('/mccoy/input/test-integer', 'r') as file_in, \
+            open_path('/mccoy/output/test-integer', 'w') as file_out:
+        test_integer_string = file_in.read()
+        test_integer = int(test_integer_string)
+        file_out.write(str(test_integer))
 
-with open_path('/mccoy/input/test-integer', 'r') as file_in, \
-        open_path('/mccoy/output/test-integer', 'w') as file_out:
-    test_integer_string = file_in.read()
-    test_integer = int(test_integer_string)
-    file_out.write(str(test_integer))
+if does_path_exist('/mccoy/input/test-float'):
+    with open_path('/mccoy/input/test-float', 'r') as file_in, \
+            open_path('/mccoy/output/test-float', 'w') as file_out:
+        test_float_string = file_in.read()
+        test_float = float(test_float_string)
+        file_out.write(str(test_float))
 
-with open_path('/mccoy/input/test-float', 'r') as file_in, \
-        open_path('/mccoy/output/test-float', 'w') as file_out:
-    test_float_string = file_in.read()
-    test_float = float(test_float_string)
-    file_out.write(str(test_float))
+if does_path_exist('/mccoy/input/test-percentage'):
+    with open_path('/mccoy/input/test-percentage', 'r') as file_in, \
+            open_path('/mccoy/output/test-percentage', 'w') as file_out:
+        test_percentage_string = file_in.read()
+        test_float = float(test_percentage_string.replace('%', ''))
+        file_out.write(str(test_float) + '%')
 
-with open_path('/mccoy/input/test-object/test-object-bool', 'r') as file_in, \
-        open_path('/mccoy/output/test-object/test-object-bool', 'w') as file_out:
-    test_bool_string = file_in.read()
-    test_bool = test_bool_string == 'True'
-    file_out.write(str(test_bool))
+if does_path_exist('/mccoy/input/test-bool'):
+    with open_path('/mccoy/input/test-bool', 'r') as file_in, \
+            open_path('/mccoy/output/test-bool', 'w') as file_out:
+        test_bool_string = file_in.read()
+        test_bool = test_bool_string == 'True'
+        file_out.write(str(test_bool))
 
-with open_path('/mccoy/input/test-object/test-object-enum', 'r') as file_in, \
-        open_path('/mccoy/output/test-object/test-object-enum', 'w') as file_out:
-    test_enum = file_in.read()
-    file_out.write(test_enum)
-
-with open_path('/mccoy/input/test-object-bool', 'r') as file_in, \
-        open_path('/mccoy/output/test-object-bool', 'w') as file_out:
-    test_bool_string = file_in.read()
-    test_bool = test_bool_string == 'True'
-    file_out.write(str(test_bool))
-
-with open_path('/mccoy/input/test-object-enum', 'r') as file_in, \
-        open_path('/mccoy/output/test-object-enum', 'w') as file_out:
-    test_enum = file_in.read()
-    file_out.write(test_enum)
+if does_path_exist('/mccoy/input/test-enum'):
+    with open_path('/mccoy/input/test-enum', 'r') as file_in, \
+            open_path('/mccoy/output/test-enum', 'w') as file_out:
+        test_enum = file_in.read()
+        file_out.write(test_enum)
 
 # # make the dir to hold test array
 # os.mkdir(build_path('/mccoy/output/test-array'))
