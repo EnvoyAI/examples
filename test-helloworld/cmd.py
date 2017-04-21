f946f32caee4c587b4700f0cca884dba83b0d8b0
Jake Taylor <jaketaylorpro@gmail.com>
2017-04-21 00:45:04 -0400

    simplifying helloworld

--- test-helloworld/cmd.py
+++ test-helloworld/cmd.py
@@ -1,11 +1,6 @@
 import sys
 
-# --------MAIN--------
-# exit, writing error message to stderr and exiting with non-zero error status if any args are passed
-if len(sys.argv) > 1:
-    sys.stderr.write('bad arguments')
-    sys.exit(1)
-# if no args were passed we read from /input and write to /output
+# read from /input and write to /output
 
 with open('/mccoy/input/hello', 'r') as file_in, \
         open('/mccoy/output/hello', 'w') as file_out:
