7627e3c7dfef000584d939039f57ac543bc1ca1f
jaketaylorpro <jaketaylorpro@gmail.com>
2017-04-24 20:32:59 -0400

    moving post-processing stuff out of echo into test-post

--- test-err/cmd.py
+++ test-err/cmd.py
@@ -11,13 +11,21 @@ with open('/mccoy/input/err', 'r') as file_in, \
         print('about to sleep for 10 minutes')
         time.sleep(600)  # sleep for 10 minutes
         print('done sleeping')
+        file_out.write(str(1))
+        exit(0)
     elif err_to_perform == 'invalid_output':
         file_out.write('non-integer')
         print('wrote a non-integer to an integer field')
-    elif err_to_perform == 'uncaught_exception':
-        print('about to raise example exception', file=sys.stderr)
-        raise Exception('example exception')
+        exit(0)
+    elif err_to_perform == 'no_output':
+        print('not writing to file')
+        exit(0)
+    elif err_to_perform == 'large_output':
+        for i in range(10000000):
+            print('aaaaaaaaaaaaaaaaaaaa' + str(i))
+        file_out.write(str(1))
+        exit(0)
     else:
-        print('exiting with code 1')
+        print('received instruction ' + err_to_perform + 'exiting with code 1')
+        file_out.write(str(1))
         exit(1)
-exit(0)
