edaf5649a92b9ac62df2ba859e8308f6daba6061
EnvoyAI Dev <dev@envoyai.com>
2017-12-10 23:24:19 -0500

    fixing small bug, and making dicom props more consistenly named

--- test-hello/cmd.py
+++ test-hello/cmd.py
@@ -2,7 +2,7 @@ import sys
 
 # read from /input and write to /output
 
-with open('/envoyai/input/hello', 'r') as file_in, \
+with open('/envoyai/input/name', 'r') as file_in, \
         open('/envoyai/output/hello', 'w') as file_out:
     test_string = file_in.read()
     file_out.write(test_string + ', come with me if you want to live.')
