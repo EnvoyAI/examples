4cc57a352268d5f1669c15534e122ed2bb5a18b1
Charley Goicoechea <chuckdesigns@gmail.com>
2018-07-25 16:08:24 -0400

    Some messaging and name cleanup

--- test-hello/cmd.py
+++ test-hello/cmd.py
@@ -5,6 +5,6 @@ import sys
 with open('/envoyai/input/name', 'r') as file_in, \
         open('/envoyai/output/hello', 'w') as file_out:
     test_string = file_in.read()
-    file_out.write(test_string + ', come with me if you want to live - modified 2.')
+    file_out.write(test_string + ', come with me if you want to live.')
 
 sys.exit(0)
